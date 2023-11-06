1bfe3d7619dd7f79957eb990add226e483e7cce5
skybloc429 <sto.lukas18@gmail.com>
2023-11-06 13:59:48 +0100

    c le debut

--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
 #include <SFML/Graphics.hpp>
 
-
-#include <SFML/Graphics.hpp>
-
 int main(int argc, char** argv)
 {
     //Création d'une fenêtre
@@ -19,7 +16,7 @@ int main(int argc, char** argv)
     //Création d'un rectangle de taille 50, 50
     sf::RectangleShape oRectangle(sf::Vector2f(50.f, 50.f));
     //A la position 100, 100
-    oCircle.setPosition(100.f, 100.f);
+    oRectangle.setPosition(100.f, 100.f);
     //Et de couleur rouge
     oRectangle.setFillColor(sf::Color::Red);
 
@@ -46,5 +43,4 @@ int main(int argc, char** argv)
     }
 
     return 0;
-    //Fuck you 
 }
